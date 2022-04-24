1c9744f0f84160499aeabeef27f2365f1d20777d
Faruqsane <85859100+Faruqsane@users.noreply.github.com>
2022-04-24 12:25:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function bagi (){
+    if(document.calc.txt.value == ''){
+        return document.calc.txt.value +='';
+    } else {
+        return document.calc.txt.value += '/';
+    }
+}
+
+function kali (){
+    if(document.calc.txt.value == ''){
+        return document.calc.txt.value +='';
+    } else {
+        return document.calc.txt.value += '*';
+    }
+}
+
+function tambah (){
+    if(document.calc.txt.value == ''){
+        return document.calc.txt.value +='';
+    } else {
+        return document.calc.txt.value += '+';
+    }
+}
+
+function kurang (){
+    if(document.calc.txt.value == ''){
+        return document.calc.txt.value +='';
+    } else {
+        return document.calc.txt.value += '-';
+    }
+}
